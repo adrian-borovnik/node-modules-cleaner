8e9cb50cfca5d0fa092b2462d87c94c56110095c
Adrian Borovnik <adrian.borovnik@gmail.com>
2025-08-30 18:29:26 +0200

    Add Cleaner

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"os"
 
-	queue "github.com/adrian-borovnik/node-modules-cleaner/pkg"
+	cleaner "github.com/adrian-borovnik/node-modules-cleaner/internal"
 )
 
 func main() {
@@ -14,50 +14,15 @@ func main() {
 		log.Fatal("Failed to get working directory")
 	}
 
-	fmt.Println("Searching for node_modules in the", workingDir)
+	c := cleaner.NewCleaner(workingDir)
 
-	nodeModules := findNodeModules(workingDir)
+	fmt.Println("Searching for node_modules in the", workingDir)
+	nodeModules := c.Search()
 
 	for _, path := range nodeModules {
 		fmt.Printf(" %s\n", path)
 	}
 
-	deleteNodeModules(nodeModules)
-}
-
-func findNodeModules(path string) []string {
-	q := queue.NewQueue[string]()
-	q.Enqueue(path)
-
-	var nodeModules []string
-	for curr, err := q.Dequeue(); err == nil; curr, err = q.Dequeue() {
-		c, err := os.ReadDir(curr)
-		if err != nil {
-			log.Fatal("Failed to read directory")
-		}
-
-		for _, entity := range c {
-			if entity.IsDir() {
-				if entity.Name() == "node_modules" {
-					nodeModules = append(nodeModules, curr+"/"+entity.Name())
-					continue
-				}
-
-				// fmt.Printf("Found directory: %s\n", curr+"/"+entity.Name())
-				q.Enqueue(curr + "/" + entity.Name())
-			}
-		}
-	}
-
-	return nodeModules
-}
-
-func deleteNodeModules(paths []string) {
-	for _, path := range paths {
-		err := os.RemoveAll(path)
-		if err != nil {
-			fmt.Println(err)
-		}
-	}
-
+	fmt.Println("Cleaning node_modules...")
+	c.Clean()
 }
--- internal/cleaner.go
+++ internal/cleaner.go
@@ -1,1 +1,59 @@
 package cleaner
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	queue "github.com/adrian-borovnik/node-modules-cleaner/pkg"
+)
+
+type Cleaner struct {
+	WorkingDir string
+	Paths      []string
+}
+
+func NewCleaner(workingDir string) *Cleaner {
+	return &Cleaner{
+		WorkingDir: workingDir,
+		Paths:      []string{},
+	}
+}
+
+func (c *Cleaner) Search() []string {
+	q := queue.NewQueue[string]()
+	q.Enqueue(c.WorkingDir)
+
+	var nodeModules []string
+	for curr, err := q.Dequeue(); err == nil; curr, err = q.Dequeue() {
+		c, err := os.ReadDir(curr)
+		if err != nil {
+			log.Fatal("Failed to read directory")
+		}
+
+		for _, entity := range c {
+			if !entity.IsDir() {
+				continue
+			}
+
+			if entity.Name() == "node_modules" {
+				nodeModules = append(nodeModules, curr+"/"+entity.Name())
+				continue
+			}
+
+			q.Enqueue(curr + "/" + entity.Name())
+		}
+	}
+
+	c.Paths = nodeModules
+	return nodeModules
+}
+
+func (c *Cleaner) Clean() {
+	for _, path := range c.Paths {
+		err := os.RemoveAll(path)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
